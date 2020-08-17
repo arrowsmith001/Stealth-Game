59a484781990ef809a09b58d71e7328cc8772074
Alex Arrowsmith <arrowsmithalexander@gmail.com>
2020-08-17 19:52:29 +0100

    changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,15 +8,27 @@ using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Animations;
+using UnityEngine.Scripting.APIUpdating;
 using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float MOVESPEED_WALK = 2;
+    public float MOVESPEED_RUN = 4;
+    public float MOVESPEED_SPRINT = 6;
+
+    private bool isCrouching = false;
+
     Animator anim;
-    Rigidbody rigidbody;
+    new Rigidbody rigidbody;
+    new CapsuleCollider collider;
+
+    CharacterController controller;
 
     private const String BOOL_IDLE = "idle";
+    private const String BOOL_WALK = "walk";
     private const String BOOL_RUN = "run";
     private const String BOOL_SPRINT = "sprint";
 
@@ -25,6 +37,10 @@ public class PlayerController : MonoBehaviour
     {
         this.anim = this.GetComponent<Animator>();
         this.rigidbody = this.GetComponent<Rigidbody>();
+        this.collider = this.GetComponent<CapsuleCollider>();
+        this.controller = this.GetComponent<CharacterController>();
+
+        rigidbody.freezeRotation = true;
     }
 
     // Start is called before the first frame update
@@ -33,13 +49,32 @@ public class PlayerController : MonoBehaviour
         
     }
 
+    bool isColliding = false;
+
     private void OnCollisionEnter(Collision collision)
     {
-        print("COLLIDED");
+        if (collision.collider.tag == "Wall")
+        {
+           // print("COLLIDED WITH WALL");
+            isColliding = true;
+        }
 
-        rigidbody.velocity = Vector3.zero;
-
-        
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.tag == "Wall")
+        {
+           // print("COLLIDING WITH WALL");
+            isColliding = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "Wall")
+        {
+            //print("COLLIDED WITH WALL");
+            isColliding = false;
+        }
 
     }
 
@@ -67,9 +102,64 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
-        float rotSpeed = 8;
-        float moveSpeed = Input.GetKey(KeyCode.LeftShift) ? 6 : 4;
+        RaycastHit upHit;
+        int ignorePlayerMask = ~(1 << 8);
 
+        if (Physics.Raycast(transform.position, transform.up, out upHit, Mathf.Infinity, ignorePlayerMask))
+        {
+            Debug.DrawLine(transform.position, upHit.point, Color.white);
+        }
+        RaycastHit fwdHit;
+        if (Physics.Raycast(transform.position, transform.up, out fwdHit, Mathf.Infinity, ignorePlayerMask))
+        {
+            Debug.DrawLine(transform.position, fwdHit.point, Color.red);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ToggleCrouching();
+        }
+
+        if (isCrouching && Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            ToggleCrouching();
+        }
+
+        Move();
+    }
+
+    private void ToggleCrouching()
+    {
+        RaycastHit upHit;
+
+        if (Physics.Raycast(transform.position, transform.up, out upHit, Mathf.Infinity, ~(1 << 8)))
+        {
+            print((upHit.collider.tag == "Wall") + " " + upHit.distance + " " + isCrouching);
+
+            if (upHit.distance < 120 && isCrouching)
+            {
+                print("STOPPED upHit: " + upHit.distance);
+                return;
+            }
+        }
+
+        isCrouching = !isCrouching;
+
+        collider.height = isCrouching ? 100 : 200;
+        collider.center = new Vector3(collider.center.x, isCrouching ? 50 : 100, collider.center.z);
+
+        controller.height = collider.height;
+        controller.center = collider.center;
+    }
+
+    private void Move()
+    {
+        // Establishes rotation speed and movespeed
+        float rotSpeed = 8;
+        float moveSpeed = isCrouching ? MOVESPEED_WALK : Input.GetKey(KeyCode.LeftShift) ? MOVESPEED_SPRINT : MOVESPEED_RUN;
+        float velocity = 0;
+
+        // Calculates forward and right vectors relative to camera
         forward = Camera.main.transform.forward;
         forward.y = 0;
         forward = Vector3.Normalize(forward);
@@ -77,45 +167,63 @@ public class PlayerController : MonoBehaviour
 
         if (Input.anyKey)
         {
-            Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); // setup a direction Vector based on keyboard input. GetAxis returns a value between -1.0 and 1.0. If the A key is pressed, GetAxis(HorizontalKey) will return -1.0. If D is pressed, it will return 1.0
 
-            if(direction != Vector3.zero)
+            // setup a direction Vector based on keyboard input.
+            // GetAxis returns a value between -1.0 and 1.0. If the A key is pressed, GetAxis(HorizontalKey) will return -1.0.
+            // If D is pressed, it will return 1.0
+            Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); 
+
+            if (direction != Vector3.zero)
             {
-                Vector3 rightMovement = right * moveSpeed
-                    //* Time.deltaTime
-                    * Input.GetAxis("Horizontal"); // Our right movement is based on the right vector, movement speed, and our GetAxis command. We multiply by Time.deltaTime to make the movement smooth.
-                Vector3 upMovement = forward * moveSpeed
-                    //* Time.deltaTime
-                    * Input.GetAxis("Vertical"); // Up movement uses the forward vector, movement speed, and the vertical axis inputs.Vector3 heading = Vector3.Normalize(rightMovement + upMovement); // This creates our new direction. By combining our right and forward movements and normalizing them, we create a new vector that points in the appropriate direction with a length no greater than 1.0transform.forward = heading; // Sets forward direction of our game object to whatever direction we're moving in
+                // Our right movement is based on the right vector, movement speed, and our GetAxis command.
+                Vector3 rightMovement = right * moveSpeed * Input.GetAxis("Horizontal");
 
-                Vector3 newPos = transform.position + rightMovement + upMovement;
-                transform.position = newPos;
+                // Up movement uses the forward vector, movement speed, and the vertical axis inputs.
+                Vector3 upMovement = forward * moveSpeed * Input.GetAxis("Vertical");
 
 
-                if (moveSpeed == 0) Animate(anim, BOOL_IDLE);
-                else if (moveSpeed == 4) Animate(anim, BOOL_RUN);
-                else Animate(anim, BOOL_SPRINT);
+                // Detect walls
+                Vector3 playerFacingWorld = transform.TransformDirection(Vector3.forward);
+                Vector3 playerLeft = transform.TransformDirection(Vector3.left);
+                Vector3 playerRight = transform.TransformDirection(Vector3.right);
+
+              
+                controller.Move(rightMovement + upMovement);
+                velocity = Vector3.Magnitude(controller.velocity);
 
 
-                Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+                Vector3 heading = Vector3.Normalize(controller.velocity);
+
                 if (heading != Vector3.zero)
                 {
-                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(heading), Time.deltaTime * rotSpeed);
+                    Quaternion newRot = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(heading), Time.deltaTime * rotSpeed);
+                    newRot.x = 0;
+                    newRot.z = 0;
+                    transform.rotation = newRot;
                 }
+
+                    
+
+                }
+
             }
             else
             {
                 Animate(anim, BOOL_IDLE);
             }
 
+
+            if (velocity <= 0.5) Animate(anim, BOOL_IDLE);
+        else if (moveSpeed == MOVESPEED_WALK) Animate(anim, BOOL_WALK);
+        else if (moveSpeed == MOVESPEED_RUN) Animate(anim, BOOL_RUN);
+        else if (moveSpeed == MOVESPEED_SPRINT) Animate(anim, BOOL_SPRINT);
+
+        //print(velocity);
+
         }
-        else
-        {
-            Animate(anim, BOOL_IDLE);
-        }
-
-
-    }
-
+        //else
+        //{
+        //    Animate(anim, BOOL_IDLE);
+        //}
     
 }
